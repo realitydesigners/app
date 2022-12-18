abb7959dab3308d9ecb1b336bd34c3b59f7c062d
Raymond <raymond.luke.spartz@gmail.com>
2022-12-17 17:38:38 -0800

    added tailwind and other packages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import sanity from "astro-sanity";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "qnse80qk",
-      dataset: "production",
-      apiVersion: "v2021-03-25",
-      useCdn: true,
-    }),
-  ],
+  integrations: [sanity({
+    projectId: "qnse80qk",
+    dataset: "production",
+    apiVersion: "v2021-03-25",
+    useCdn: true
+  }), tailwind()]
 });
