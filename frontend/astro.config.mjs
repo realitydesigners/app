90c42e9bed348ef43d0026a9ea41e55281f70c31
Raymond <raymond.luke.spartz@gmail.com>
2022-12-17 17:35:16 -0800

    added astro-sanity

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import sanity from "astro-sanity";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  integrations: [
+    sanity({
+      projectId: "qnse80qk",
+      dataset: "production",
+      apiVersion: "v2021-03-25",
+      useCdn: true,
+    }),
+  ],
 });
