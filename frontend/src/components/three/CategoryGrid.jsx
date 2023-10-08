1719f0b0196f075f468774bf4e6925f9fb729916
Raymond <raymond.luke.spartz@gmail.com>
2023-10-07 17:27:56 -0700

    further improve design

--- frontend/src/components/three/CategoryGrid.jsx
+++ frontend/src/components/three/CategoryGrid.jsx
@@ -5,36 +5,6 @@ import AllCategories from './AllCategories'
 import { useCategory } from './CategoryContext'
 import Stars from './Stars'
 
-const Description = () => {
-  return (
-    <div className="w-full lg:w-1/4top-20 left-8 flex flex-col h-full p-8 backdrop-blur-sm  rounded-lg shadow-lg pb-20">
-      <h2 className="text-gray-200  text-4xl font-extrabold mb-2 leading-tight">
-        Topic Exploration
-      </h2>
-      <p className="text-gray-300   text-md leading-tight mb-4 opacity-80">
-        A 3D expanding grid of categories with hover effects and spatial
-        popovers.
-      </p>
-      <h3 className="text-gray-200 text-2xl font-bold mb-2 leading-snug">
-        Data Integration
-      </h3>
-      <p className="text-gray-300 rajdhani text-md leading-tight mb-4 opacity-80">
-        The visualization is made using react-three-fiber. All content is
-        sourced from our Sanity API ensuring a real-time experience.
-      </p>
-      <h3 className="text-gray-200  text-xl font-bold mb-2 leading-snug">
-        Use Cases
-      </h3>
-      <ul className="text-gray-300  text-md mb-2 leading-tight pl-8 list-disc opacity-80">
-        <li className="mb-1">
-          Interactive Blog Exploration: Topics are shown as an object that can
-          be hovered and show related topics from the main.
-        </li>
-      </ul>
-    </div>
-  )
-}
-
 const InteractiveWorldScene = ({ category = [] }) => {
   const { navigation, setNavigation } = useCategory()
 
@@ -84,9 +54,10 @@ const InteractiveWorldScene = ({ category = [] }) => {
   )
 
   return (
-    <div className="relative flex flex-col-reverse lg:flex-row justify-center items-center w-full h-auto p-0 lg:p-20 gap-8">
+    <div className="relative flex flex-wrap flex-col-reverse lg:flex-row-reverse justify-center  w-full h-auto   border border-gray-600 mb-20 ">
+      <TopBar />
       <Description />
-      <div className="w-full lg:w-3/4 flex lg:h-[80vh] h-[50vh] border border-gray-200 lg:rounded-xl shadow-2xl">
+      <div className="w-full lg:w-3/4 flex lg:h-[90vh] h-[80vh]  shadow-2xl border-b lg:border-b-0 lg:border-r border-gray-600">
         <Canvas
           camera={{ position: [0, 0, 40], fov: 45 }}
           style={{ height: 'full', width: '100vw', background: 'black' }}
@@ -103,8 +74,60 @@ const InteractiveWorldScene = ({ category = [] }) => {
           <Stars />
         </Canvas>
       </div>
+      <Bar />
     </div>
   )
 }
 
 export default InteractiveWorldScene
+
+const Description = () => {
+  return (
+    <div className="w-full lg:w-1/4 font-mono flex flex-col h-full p-8 b border-b border-gray-600 shadow-lg ">
+      <div class="flex flex-row text-xs font-bold gap-2 mb-8 ">
+        <span class="text-teal-200 flex w-auto bg-teal-200/50  rounded pl-2 pr-2 p-1 border border-teal-200">
+          3D
+        </span>
+        <span class="text-yellow-200 flex w-auto bg-yellow-200/50  rounded pl-2 pr-2 p-1 border border-yellow-200">
+          CMS
+        </span>
+      </div>
+      <h2 className="text-gray-200  text-3xl font-extrabold mb-2 leading-tight">
+        Topic Exploration
+      </h2>
+      <p className="text-gray-300  text-sm leading-tight mb-12 opacity-80">
+        A spatial representation of each topic with hover effects to reveal
+        related topics.
+      </p>
+
+      <h3 className="text-gray-200   text-lg font-bold mb-2 leading-snug">
+        Use Cases
+      </h3>
+      <ul className="text-gray-300  text-sm mb-2 leading-tight pl-8 list-disc opacity-80">
+        <li className="mb-1">
+          Explore topics and subtopics in a spatial representation, with popover
+          and hover effects to reveal content.
+        </li>
+      </ul>
+      <div class="flex flex-row text-xs font-bold gap-2 my-4">
+        <span class="text-black flex w-auto te bg-white p-2">
+          React-Three-Fiber
+        </span>
+        <span class="text-black flex w-auto bg-white p-2">Sanity</span>
+      </div>
+    </div>
+  )
+}
+
+const Bar = () => {
+  return (
+    <div className="w-full h-8 flex text-gray-600 font-mono uppercase tracking-widest text-xs items-center justify-center lg:border-t border-gray-600"></div>
+  )
+}
+const TopBar = () => {
+  return (
+    <div className="w-full h-8 flex text-gray-600 font-mono uppercase tracking-widest text-xs items-center justify-center lg:border-b border-gray-600">
+      <p>Reality Designers | Topic Grid </p>
+    </div>
+  )
+}
--- frontend/src/components/three/TunnelPosts.jsx
+++ frontend/src/components/three/TunnelPosts.jsx
@@ -4,38 +4,6 @@ import { useRef } from 'react'
 import PostsList from './PostsList'
 import Stars from './Stars'
 
-const Description = () => {
-  return (
-    <div className="w-full lg:w-1/4top-20 left-8 flex flex-col h-full p-8 backdrop-blur-sm rounded-lg shadow-lg pb-20">
-      <h2 className="text-gray-200  text-4xl font-extrabold mb-2 leading-tight">
-        Tunnel
-      </h2>
-      <p className="text-gray-300  text-md leading-tight mb-4 opacity-80">
-        A spatial representation where each post transforms into a navigable box
-        within a cosmic tunnel.
-      </p>
-      <h3 className="text-gray-200  text-2xl font-bold mb-2 leading-snug">
-        Data Integration
-      </h3>
-      <p className="text-gray-300  rajdhani text-md leading-tight mb-4 opacity-80">
-        The visualization is made using react-three-fiber. The content including
-        the images, title, and other meta information is sourced from our Sanity
-        API ensuring a real-time experience.
-      </p>
-      <h3 className="text-gray-200   text-xl font-bold mb-2 leading-snug">
-        Use Cases
-      </h3>
-      <ul className="text-gray-300  text-md mb-2 leading-tight pl-8 list-disc opacity-80">
-        <li className="mb-1">
-          Interactive Blog Exploration: Posts are shown as a card in an ethereal
-          gallery, with hover effects and popovers when interacting with the
-          post.
-        </li>
-      </ul>
-    </div>
-  )
-}
-
 const Tunnel = ({ posts }) => {
   return (
     <>
@@ -46,9 +14,10 @@ const Tunnel = ({ posts }) => {
 
 const Scene = ({ posts }) => {
   return (
-    <div className="relative flex flex-col-reverse lg:flex-row-reverse justify-center items-center w-full h-auto p-0 lg:p-20 gap-8 ">
+    <div className="relative flex flex-wrap flex-col-reverse lg:flex-row-reverse justify-center  w-full h-auto   border border-gray-600 mb-20 ">
+      <TopBar />
       <Description />
-      <div className="w-full lg:w-3/4 flex lg:h-[80vh] h-[50vh] shadow-2xl border border-gray-200 lg:rounded-xl">
+      <div className="w-full lg:w-3/4 flex lg:h-[90vh] h-[80vh]  shadow-2xl border-b lg:border-b-0 lg:border-r border-gray-600">
         <Canvas
           camera={{ position: [0, 0, 20], fov: 45 }}
           style={{ height: 'full', width: '100vw', background: 'black' }}
@@ -60,8 +29,61 @@ const Scene = ({ posts }) => {
           <Tunnel posts={posts} />
         </Canvas>
       </div>
+      <Bar />
     </div>
   )
 }
 
 export default Scene
+
+const Description = () => {
+  return (
+    <div className="w-full lg:w-1/4 font-mono flex flex-col h-full p-6 b border-b border-gray-600 shadow-lg ">
+      <div class="flex flex-row text-xs font-bold gap-2 mb-8 ">
+        <span class="text-teal-200 flex w-auto bg-teal-200/50  rounded pl-2 pr-2 p-1 border border-teal-200">
+          3D
+        </span>
+        <span class="text-yellow-200 flex w-auto bg-yellow-200/50  rounded pl-2 pr-2 p-1 border border-yellow-200">
+          CMS
+        </span>
+      </div>
+      <h2 className="text-gray-200  text-3xl font-extrabold mb-2 leading-tight">
+        Tunnel
+      </h2>
+      <p className="text-gray-300  text-sm leading-tight mb-12 opacity-80">
+        A spatial representation where each post transforms into a navigable box
+        within a cosmic tunnel.
+      </p>
+      <h3 className="text-gray-200   text-lg font-bold mb-2 leading-snug">
+        Use Cases
+      </h3>
+      <ul className="text-gray-300  text-sm mb-2 leading-tight pl-8 list-disc opacity-80 ">
+        <li className="mb-1">
+          Interactive Blog Exploration: Posts are shown as a card in an ethereal
+          gallery, with hover effects and popovers when interacting with the
+          post.
+        </li>
+      </ul>
+      <div class="flex flex-row text-xs font-bold gap-2 my-4">
+        <span class="text-black flex w-auto te bg-white p-2">
+          React-Three-Fiber
+        </span>
+        <span class="text-black flex w-auto bg-white p-2">Sanity</span>
+      </div>
+    </div>
+  )
+}
+
+const Bar = () => {
+  return (
+    <div className="w-full h-8 flex text-gray-600 font-mono uppercase tracking-widest text-xs items-center justify-center lg:border-t border-gray-600"></div>
+  )
+}
+
+const TopBar = () => {
+  return (
+    <div className="w-full h-8 flex text-gray-600 font-mono uppercase tracking-widest text-xs items-center justify-center lg:border-b border-gray-600">
+      <p>Reality Designers | Tunnel </p>
+    </div>
+  )
+}
