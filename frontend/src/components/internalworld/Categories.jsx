83b59b23c684ee4e4245c16a667e7fd6fc60a4fd
Raymond <raymond.luke.spartz@gmail.com>
2023-10-18 13:13:27 -0700

    added internal world base

---
+++ frontend/src/components/internalworld/Categories.jsx
@@ -0,0 +1,75 @@
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useCallback, useState } from 'react'
+import AllCategories from './AllCategories'
+import { useCategory } from './CategoryContext'
+
+const Categories = ({ category = [] }) => {
+  const { navigation, setNavigation } = useCategory()
+
+  // State variables
+  const [highlightedWorld, setHighlightedWorld] = useState(null)
+
+  const [selectedMainWorld, setSelectedMainWorld] = useState(null)
+  const [highlightedCategory, setHighlightedCategory] = useState(null)
+
+  const handleMainWorldInteraction = useCallback(
+    (worldName, position, childCategoryName) => {
+      setSelectedMainWorld(worldName)
+      setHighlightedWorld(worldName)
+      setHighlightedCategory(worldName)
+
+      const currentCategory = category.find((cat) => cat.title === worldName)
+      if (currentCategory && currentCategory.sceneIdentifier) {
+        setActiveBackgroundScene(currentCategory.sceneIdentifier)
+      }
+
+      if (currentCategory && currentCategory.associatedPosts) {
+        setCurrentContent(currentCategory.associatedPosts)
+      } else {
+        setCurrentContent([])
+      }
+
+      setSidebarOpen(true)
+
+      if (childCategoryName) {
+        setHighlightedWorld(childCategoryName)
+        setSelectedSubWorld(childCategoryName)
+      } else {
+        setSelectedSubWorld(null)
+      }
+
+      setNavigation((prev) => ({
+        ...prev,
+        mainWorld: worldName,
+        category: prev.category,
+      }))
+    },
+    [category, setNavigation],
+  )
+
+  const mainCategories = category.filter(
+    (cat) => Boolean(cat.title) && cat.isMain,
+  )
+
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Canvas
+        camera={{ position: [0, 0, 40], fov: 45 }}
+        style={{ height: '100vh', width: '100vw', background: 'black' }}
+        className="border  border-black"
+      >
+        <OrbitControls />
+        <AllCategories
+          categories={mainCategories}
+          highlightedCategory={highlightedCategory}
+          handleMainWorldInteraction={handleMainWorldInteraction}
+          selectedMainWorld={selectedMainWorld}
+          highlightedWorld={highlightedWorld}
+        />
+      </Canvas>
+    </div>
+  )
+}
+
+export default Categories
