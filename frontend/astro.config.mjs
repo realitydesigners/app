de7d7699095f314efdd1a1fec22742dcc863e234
RD <raymond.luke.spartz@gmail.com>
2023-12-05 16:17:08 -0800

    new astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,27 +1,27 @@
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import sanity from 'astro-sanity'
-import { defineConfig, passthroughImageService } from 'astro/config'
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sanity from 'astro-sanity';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    sanity({
-      projectId: 'fovvfda4',
-      dataset: 'production',
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  images: {
-    domains: ['cdn.sanity.io'],
-  },
-  image: {
-    service: passthroughImageService(),
-  },
-  plugins: ['prettier-plugin-astro'],
-  prefetch: true,
-})
+   integrations: [
+      tailwind(),
+      react(),
+      sanity({
+         projectId: 'fovvfda4',
+         dataset: 'production',
+      }),
+   ],
+   server: {
+      port: 3000,
+   },
+   images: {
+      domains: ['cdn.sanity.io'],
+   },
+   image: {
+      service: passthroughImageService(),
+   },
+   plugins: ['prettier-plugin-astro'],
+   prefetch: true,
+});
